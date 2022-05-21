18239410bab775bcf54ed0cdd4ec357f39c73f95
Maksym Severyn <severinmaxim2015@gmail.com>
2022-05-21 17:12:02 +0200

    Concurrent initial dataloading added

--- src/main/java/com/example/breweriesapp/BreweryService.java
+++ src/main/java/com/example/breweriesapp/BreweryService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class BreweryService {
@@ -52,27 +54,29 @@ public class BreweryService {
     }
 
     public void initialDataLoading() {
-        if (breweriesRepo.count() > 0) {
+        if (breweriesRepo.count() < 1) {
             System.out.println("Initialize data not needed - DB is not empty!");
-        } else {
-            System.out.println("Data getting from CSV...");
-            List<String[]> strings = CsvParser.getFile();
-            List<BreweryEntity> breweryEntities = strings
-                    .stream()
-                    .map(fromArraysToEntityMapper::map).toList();
-
-            Long manualId = 1L;
-            for (BreweryEntity breweryEntity : breweryEntities) {
-                List<WorkingDay> hours = breweryEntity.getHours();
-                for (WorkingDay day : hours) {
-                    day.setId(manualId);
-                    day.setOwnedBreweryEntity(breweryEntity);
-                    manualId++;
-                }
-            }
-            System.out.println("Data started to loading to DB, please wait... The first initialization may take longer");
-            breweriesRepo.saveAll(breweryEntities);
-            System.out.println("Data loaded to DB!");
+            return;
         }
+        System.out.println("Data getting from CSV...");
+        List<String[]> strings = CsvParser.getFile();
+        List<BreweryEntity> breweryEntities = strings
+                .stream()
+                .map(fromArraysToEntityMapper::map).toList();
+
+        Long manualId = 1L;
+        for (BreweryEntity breweryEntity : breweryEntities) {
+            List<WorkingDay> hours = breweryEntity.getHours();
+            for (WorkingDay day : hours) {
+                day.setId(manualId);
+                day.setOwnedBreweryEntity(breweryEntity);
+                manualId++;
+            }
+        }
+        System.out.println("Data started to loading to DB...");
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        executorService.submit(() -> breweriesRepo.saveAll(breweryEntities));
+        executorService.shutdown();
+        System.out.println("Data loaded to DB!");
     }
 }
